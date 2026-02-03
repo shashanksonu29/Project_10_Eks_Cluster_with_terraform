c44c827fbedff354e047e05c26cee5cdaaffb92b
shashanksonu29 <shashanksonu29@gmail.com>
2026-02-03 18:20:14 +0530

    Update main.tf

--- eks-install/backend/main.tf
+++ eks-install/backend/main.tf
@@ -30,7 +30,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" 
 resource "aws_dynamodb_table" "terraform_locks" {
   name         = "my-todo-eks-state-lock"
   billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
+  hash_key     = "LockkID"
 
   attribute {
     name = "LockID"
